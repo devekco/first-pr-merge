8f4d030badc02bd06c48c4053520470ff75f6565
Mark Groves <mark@markgroves.us>
2018-01-28 12:27:35 -0800

    fixed bug did not need defaultContext

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ module.exports = robot => {
 
       if (mergedPRs.length === 0) {
         try {
-          const config = await robot.config.get(context, defaultConfig);
+          const config = await robot.config.get(context);
           
           if (config.firstPRMergeComment) {
             context.github.issues.createComment(context.issue({body: config.firstPRMergeComment}))
